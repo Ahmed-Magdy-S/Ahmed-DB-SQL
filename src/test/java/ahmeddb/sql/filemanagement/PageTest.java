be1a31292c4af0a11bcf68a77766d78f0fa3001b
Ahmed-Magdy-S <ahmed.magdy.s.19@gmail.com>
2024-01-29 17:18:10 +0200

    add tests for Page class

---
+++ src/test/java/ahmeddb/sql/filemanagement/PageTest.java
@@ -0,0 +1,106 @@
+package ahmeddb.sql.filemanagement;
+
+import ahmeddb.sql.configuration.DataSourceConfigProvider;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test cases for page class
+ */
+class PageTest {
+
+    @Test
+    void getInt() {
+        //arrange
+        Page page = new Page();
+        page.setInt(100,19);
+        page.setInt(104,2000);
+
+        //act & assert
+        assertEquals(19,page.getInt(100));
+        assertEquals(2000,page.getInt(104));
+
+    }
+
+    @Test
+    void setInt() {
+        //arrange
+        Page page = new Page();
+
+        //act
+        page.setInt(100,19);
+        page.setInt(104,2000);
+
+        //assert
+        assertEquals(19,page.getInt(100));
+        assertEquals(2000,page.getInt(104));
+    }
+
+    @Test
+    void setString() {
+
+        //arrange
+        Page page = new Page();
+        String str = "Ahmed";
+        int byteBufferIndex = 0;
+        int stringBytesLength = page.getStringBytesLength(str);
+
+        //act
+        page.setString(byteBufferIndex,str);
+        page.setString(byteBufferIndex + Integer.BYTES + stringBytesLength,"test");
+        int actualStringBytesLength = page.getInt(byteBufferIndex);
+        String actualString = page.getString(byteBufferIndex);
+        String actualString2 = page.getString(byteBufferIndex + Integer.BYTES + stringBytesLength);
+
+
+        //assert
+        assertEquals(stringBytesLength,actualStringBytesLength);
+        assertEquals(str,actualString);
+        assertEquals("test",actualString2);
+
+    }
+
+    @Test
+    void getString() {
+        //arrange
+        Page page = new Page();
+        String str = "Ahmed";
+        int byteBufferIndex = 0;
+        int stringBytesLength = page.getStringBytesLength(str);
+
+        //act
+        page.setString(byteBufferIndex,str);
+        page.setString(byteBufferIndex + Integer.BYTES + stringBytesLength,"test");
+        int actualStringBytesLength = page.getInt(byteBufferIndex);
+        String actualString = page.getString(byteBufferIndex);
+        String actualString2 = page.getString(byteBufferIndex + Integer.BYTES + stringBytesLength);
+
+
+        //assert
+        assertEquals(stringBytesLength,actualStringBytesLength);
+        assertEquals(str,actualString);
+        assertEquals("test",actualString2);
+    }
+
+    @Test
+    void getByteBuffer() {
+        //arrange
+        Page page = new Page();
+        Page page2 = new Page(new byte[10]);
+        int dbBlockSize = DataSourceConfigProvider.getDataSourceConfig().getBlockSize();
+
+        //act
+        int pageBlockSize = page.getByteBuffer().capacity();
+
+        //assert
+        assertEquals(dbBlockSize, pageBlockSize);
+        assertTrue(page.getByteBuffer().isDirect());
+        assertFalse(page2.getByteBuffer().isDirect());
+        assertNotEquals(page,page2);
+        assertFalse(page.getByteBuffer().isReadOnly());
+        assertFalse(page2.getByteBuffer().isReadOnly());
+
+    }
+}
